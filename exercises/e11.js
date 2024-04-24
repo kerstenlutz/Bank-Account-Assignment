525fd324292a26e232db81805c6a328f4c15e546
Kersten Lutz <kerstenlutz@hotmail.com>
2024-04-24 10:20:22 -0400

    Exercise 11 commit

--- exercises/e11.js
+++ exercises/e11.js
@@ -4,10 +4,26 @@
 // Array example: bankAccounts in /data/data.js
 // getAllWithdrawals(bankAccounts) => [3432, 0, 43242.34, 0, 23432]
 
-export function getAllWithdrawals(array) {
-  // Your code goes here...
+import { bankAccounts } from "../data/data";
 
+export function getAllWithdrawals(array) {
+  let result = [];
+  let sum = 0;
+  for (let elem in bankAccounts) {
+    if (bankAccounts[elem].withdrawals > [0]) {
+      sum = 0;
+      for (let i = 0; i < bankAccounts[elem].withdrawals.length; i++) {
+        sum += bankAccounts[elem].withdrawals[i];
+      }
+      result.push(sum);
+    } else if (bankAccounts[elem].withdrawals != [0]) {
+      result.push(0);
+    }
+  }
+  return result;
 }
+
+getAllWithdrawals(bankAccounts);
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-11"
